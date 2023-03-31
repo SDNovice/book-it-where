659bc3a655641b8f9189b4574912503b2152bae9
Sdby2003 <115440321+Sdby2003@users.noreply.github.com>
2023-03-31 13:11:44 -0500

    added middleware to server.js

--- server/server.js
+++ server/server.js
@@ -3,8 +3,18 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const { authMiddleware } = require('./utils/auth');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +24,22 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 app.use(routes);
 
-db.once('open', () => {
+
+
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+  };
+  
+// Call the async function to start the server
+  startApolloServer(typeDefs, resolvers);
